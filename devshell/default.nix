7a585ad3acada1731f00f5e9db2b733f952e66af
GTrunSec <gtrunsec@hardenedlinux.org>
2022-03-15 14:27:57 -0700

    fmt

--- devshell/default.nix
+++ devshell/default.nix
@@ -8,6 +8,5 @@
     ./commands.toml
     ./lambad-microvm-hunting-lab.nix
     ./sops.toml
-    inputs.cells."x86_64-linux".common.devshellProfiles.default
   ];
 }
--- devshell/lambad-microvm-hunting-lab.nix
+++ devshell/lambad-microvm-hunting-lab.nix
@@ -1,7 +1,9 @@
 {
   pkgs,
   extraModulesPath,
+  inputs,
   ...
 }: {
   packages = [pkgs.ragenix];
+  imports = [inputs.cells."x86_64-linux".update.devshellProfiles.default]; #
 }
