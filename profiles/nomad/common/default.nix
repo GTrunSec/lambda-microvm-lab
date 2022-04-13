3c67a9355b68fc888aa13840e2070c4badbfd792
GTrunSec <gtrunsec@hardenedlinux.org>
2022-04-13 14:06:20 -0700

    fix infinite recursion encountered

--- profiles/nomad/common/default.nix
+++ profiles/nomad/common/default.nix
@@ -6,9 +6,5 @@
 }: {
   imports = [
     inputs.nomad-driver.nixosModules.nix-driver-nomad
-    "${pkgs}/nixos/modules/misc/version.nix"
-    "${pkgs}/nixos/modules/profiles/base.nix"
-    "${pkgs}/nixos/modules/profiles/headless.nix"
-    "${pkgs}/nixos/modules/profiles/minimal.nix"
   ];
 }
